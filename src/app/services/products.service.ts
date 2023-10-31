c68d4b974d4a99d27ba3b628204a6cd53af07abd
alejandro1502 <mateo@MacBook-Pro-de-Mateo.local>
2023-10-30 23:33:06 -0500

    class 10 observable-promise \ class 11 configuracion proxy \ class 12 Manejo de ambientes

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
+
+import {environment} from './../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products'
+  private apiUrl = `${environment.API_URL}/api/products`;
 
   constructor(
     private http: HttpClient
@@ -20,7 +23,8 @@ private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products'
       params = params.set('limit',limit)
       params = params.set('offset',limit)
     }
-    return this.http.get<Product[]>(this.apiUrl,{params});
+    return this.http.get<Product[]>(this.apiUrl,{params})
+    .pipe(retry(3));
   }
 
   getProduct(id: string) {
