56b3015b27b14e8441c9af56559af62ee5669746
alejandro1502 <mateo@MacBook-Pro-de-Mateo.local>
2023-11-03 22:37:00 -0500

    Login and auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { Product } from './models/product.model';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,11 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  constructor(
+    private authService : AuthService,
+    private usersService : UsersService,
+  ){}
+
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -18,5 +24,23 @@ export class AppComponent {
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser(){
+    this.usersService.create({
+      name:'alejo',
+      email:'alejo@gmail.com',
+      password:'1212'
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    })
+  }
+
+  login(){
+    this.authService.login('alejo@gmail.com','1212')
+    .subscribe(rta => {
+      console.log(rta.access_token);
+    })
   }
 }
